b98daaf4b558e1cc392c99e208fb82799cdcff99
jessevictor <kongoijesse97@gmail.com>
2020-07-18 22:01:24 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+var weekDays =['Sunday', 'Monday','Tuesday','Wenesday','Thursday','Friday','Saturday'];
+var maleNames =['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
+var femaleNames =['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
+
+
 function validateForm() {
-    var x = document.forms["myForm"]["fname"].value;
+    var d = parseInt(document.getElementById('d').value);
+    var m = parseInt(document.getElementById('m').value);
+    var y = parseInt(document.getElementById('y').value);
     if (x == "") {
       alert("Name must be filled out");
       return false;
