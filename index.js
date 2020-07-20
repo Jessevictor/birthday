f2a377b8773f0771e5b9f7b5be19dda1691318cd
jessevictor <kongoijesse97@gmail.com>
2020-07-20 18:19:24 +0300

    add footer

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
-var weekDays =['Sunday', 'Monday','Tuesday','Wenesday','Thursday','Friday','Saturday'];
-var maleNames =['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
-var femaleNames =['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
+ 
+function checkName() {
+    var dd, mm, yyyy;
+    dd = document.getElementById("dd").value;
+    mm = document.getElementById("mm").value;
+    yyyy = document.getElementById("yyyy").value;
+
+    var cc = parseInt(yyyy.slice(0, 2));
+    var yy = parseInt(yyyy.slice(2, 4));
 
 
-function validateForm() {
-    var d = parseInt(document.getElementById('d').value);
-    var m = parseInt(document.getElementById('m').value);
-    var y = parseInt(document.getElementById('y').value);
-    if (x == "") {
-      alert("Name must be filled out");
-      return false;
+    if (document.getElementById("inlineRadioMale").checked) {
+        gender = "Male";
+    } else if (document.getElementById("inlineRadioFemale").checked) {
+        gender = "Female";
+    } else (alert("Please choose gender"));
+
+
+    if (dd < 1 && dd > 31) {
+        alert("Please enter a valid date");
     }
+
+    if (mm < 1 && mm > 12) {
+        alert("Please enter a valid month");
+    }
+
+    var f = parseInt(((cc / 4) - ((2 * cc) - 1) + ((5 * yy) / 4) + (26 * (mm + 1) / 10) + dd) % 7);
+    alert(f)
+
+    var femaleNames = ["Akosua","Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+
+    var maleNames = ["Kwasi","Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+
+    var daysofWeek = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+
+    //TODO: Read Use javascript Date(), use momentJs
+
+
+
+    if (gender == "Female") {
+        alert("Your name is " + femaleNames[f - 1] + " You were born on " + daysofWeek[f - 1])
+        return;
+    }
+
+    if (gender == "Male") {
+        alert("Your name is " + meleNames[f - 1] + " You were born on " + daysofWeek[f - 1])
+        return;
+    }
+
+
+}
+
